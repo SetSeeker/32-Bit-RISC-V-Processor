f08feff0a6023e9837a971134917b9c6721bd828
nathancook9 <nathan.d.cook99@gmail.com>
2025-02-02 17:59:54 -0500

    Better version

--- ELEC374_PHASE1/add_tb.v
+++ ELEC374_PHASE1/add_tb.v
@@ -1,11 +1,11 @@
-// add_tb.v file: Testbench for mul R2, R6
+// add_tb.v file: Testbench for add R4, R3 and R7
 `timescale 1ns/10ps
 
 module add_tb;
     // Control signals
     reg PCout, Zlowout, Zhighout, MDRout, R2out, R6out;
     reg MARin, Zin, PCin, MDRin, IRin, Yin, LOin, HIin;
-    reg IncPC, Read, ADD;
+    reg IncPC, Read, ADD, R3in, R4in, R7in;
     reg Clock;
     reg [31:0] Mdatain;
 
@@ -36,13 +36,18 @@ module add_tb;
     // Finite state machine
     always @(posedge Clock) begin
         case (Present_state)
-            Default: Present_state = T0;
-            T0: Present_state = T1;
-            T1: Present_state = T2;
-            T2: Present_state = T3;
-            T3: Present_state = T4;
-            T4: Present_state = T5;
-            T5: Present_state = Default; // Reset to Default after completion
+            Default     : Present_state = Reg_load1a;
+            Reg_load1a  : Present_state = Reg_load1b;
+            Reg_load1b  : Present_state = Reg_load2a;
+            Reg_load2a  : Present_state = Reg_load2b;
+            Reg_load2b  : Present_state = Reg_load3a;
+            Reg_load3a  : Present_state = Reg_load3b;
+            Reg_load3b  : Present_state = T0;
+            T0          : Present_state = T1;
+            T1          : Present_state = T2;
+            T2          : Present_state = T3;
+            T3          : Present_state = T4;
+            T4          : Present_state = T5;
         endcase
     end
 
@@ -50,45 +55,72 @@ module add_tb;
     always @(Present_state) begin
         case (Present_state)
             Default: begin
-                // Initialize all control signals to 0
-                PCout <= 0; Zlowout <= 0; Zhighout <= 0; MDRout <= 0;
-                R2out <= 0; R6out <= 0; MARin <= 0; Zin <= 0;
+                // Initialize all control signals
+                PCout <= 0; Zlowout <= 0; MDRout <= 0;
+                R3out <= 0; R7out <= 0; MARin <= 0; Zin <= 0;
                 PCin <= 0; MDRin <= 0; IRin <= 0; Yin <= 0;
-                LOin <= 0; HIin <= 0; IncPC <= 0; Read <= 0; ADD <= 0;
+                IncPC <= 0; Read <= 0; ADD <= 0;
+                R3in <= 0; R4in <= 0; R7in <= 0;
                 Mdatain <= 32'h00000000;
             end
 
+            // Load R3 with 0x22
+            Reg_load1a: begin
+                Mdatain <= 32'h00000022;
+                Read <= 1; MDRin <= 1;
+                #15 Read <= 0; MDRin <= 0;
+            end
+            Reg_load1b: begin
+                MDRout <= 1; R3in <= 1;
+                #15 MDRout <= 0; R3in <= 0;
+            end
+
+            // Load R7 with 0x24
+            Reg_load2a: begin
+                Mdatain <= 32'h00000024;
+                Read <= 1; MDRin <= 1;
+                #15 Read <= 0; MDRin <= 0;
+            end
+            Reg_load2b: begin
+                MDRout <= 1; R7in <= 1;
+                #15 MDRout <= 0; R7in <= 0;
+            end
+
+            // Load R4 with 0x28 (destination register)
+            Reg_load3a: begin
+                Mdatain <= 32'h00000028;
+                Read <= 1; MDRin <= 1;
+                #15 Read <= 0; MDRin <= 0;
+            end
+            Reg_load3b: begin
+                MDRout <= 1; R4in <= 1;
+                #15 MDRout <= 0; R4in <= 0;
+            end
+
+            // Instruction Fetch: Fetch the opcode for "add R4, R3, R7"
             T0: begin
-                // Step T0: PCout, MARin, IncPC, Zin
                 PCout <= 1; MARin <= 1; IncPC <= 1; Zin <= 1;
             end
 
             T1: begin
-                // Step T1: Zlowout, PCin, Read, Mdatain[31..0], MDRin
                 Zlowout <= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-                Mdatain <= 32'h00062020; // Opcode: mul R2, R6
+                Mdatain <= 32'h2A2A8000; // Opcode for "add R4, R3, R7"
             end
 
             T2: begin
-                // Step T2: MDRout, IRin
                 MDRout <= 1; IRin <= 1;
             end
 
             T3: begin
-                // Step T3: R2out, Yin
-                R2out <= 1; Yin <= 1;
+                R3out <= 1; Yin <= 1;
             end
 
             T4: begin
-                // Step T4: R6out, ADD, Zin
-                R6out <= 1;
-                
-                Zin <= 1;
+                R7out <= 1; ADD <= 1; Zin <= 1;
             end
 
             T5: begin
-                // Step T5: Zlowout, R2in
-                Zlowout <= 1; R2out <= 1;
+                Zlowout <= 1; R4in <= 1;
             end
 
         endcase
