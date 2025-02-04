47a68c755edab7133017c4d0ae8fbd352daf7058
CyberJunky <semyonskripchenko@gmail.com>
2025-02-04 16:33:47 -0500

    ALU, Bus and Datapath fix

--- ELEC374_PHASE1/DataPath.v
+++ ELEC374_PHASE1/DataPath.v
@@ -1,41 +1,106 @@
 module DataPath #(parameter DATA_WIDTH = 32)(
 	input Clock, Clear,
 	input R3in, R4in, R7in, Zin, PCin, MDRin, IRin, Yin, MARin,
-	input IncPC, Read, AND,
-	input[DATA_WIDTH-1:0] Mdatain,
-	//input [3:0] control,
-	input R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out,
-	R11out, R12out, R13out, R14out, R15out, PCout, Zlowout, MDRout
+	input IncPC, Read,
+	input [DATA_WIDTH-1:0] Mdatain,
+	input [3:0] control,
+	input R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, 
+          R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, 
+          PCout, Zlowout, MDRout, HIout, LOout, Z_high_out, Z_low_out,
+          In_Port_out, C_sign_extended_out
 );
 
 	wire [DATA_WIDTH-1:0] R0, R1, R2, R3, R4, 
-						  R5, R6, R7, R8, R9, 
-						  R10, R11, R12, R13, R14, R15,
-						  alu_result;
+                          R5, R6, R7, R8, R9, 
+                          R10, R11, R12, R13, R14, R15,
+                          HI, LO, Z_high, Z_low,
+                          PC, In_Port, C_sign_extended,
+                          alu_result, BusMuxOut, BusMuxIn_MDR;
 
 	//Devices
 	register #(DATA_WIDTH) reg3 (
-		.clock(clock),
-		.clear(clear),
+		.clock(Clock),
+		.clear(Clear),
 		.enable(R3in),
 		.data_in(Mdatain),
 		.data_out(R3)
 	);
 
 	register #(DATA_WIDTH) reg7 (
-		.clock(clock),
-		.clear(clear),
+		.clock(Clock),
+		.clear(Clear),
 		.enable(R7in),
 		.data_in(Mdatain),
 		.data_out(R7)
 	);
 
+	register #(DATA_WIDTH) pc_register (
+        .clock(Clock),
+        .clear(Clear),
+        .enable(PCin),
+        .data_in(Mdatain),
+        .data_out(PC)
+    );
+
+    // HI Register
+    register #(DATA_WIDTH) hi_register (
+        .clock(Clock),
+        .clear(Clear),
+        .enable(Zin), 
+        .data_in(alu_result),
+        .data_out(HI)
+    );
+
+    // LO Register
+    register #(DATA_WIDTH) lo_register (
+        .clock(Clock),
+        .clear(Clear),
+        .enable(Zin), 
+        .data_in(alu_result),
+        .data_out(LO)
+    );
+
+    // Z High and Z Low Registers
+    register #(DATA_WIDTH) z_high_register (
+        .clock(Clock),
+        .clear(Clear),
+        .enable(Zin),
+        .data_in(alu_result[63:32]),
+        .data_out(Z_high)
+    );
+
+    register #(DATA_WIDTH) z_low_register (
+        .clock(Clock),
+        .clear(Clear),
+        .enable(Zin),
+        .data_in(alu_result[31:0]),
+        .data_out(Z_low)
+    );
+
+    // In_Port (For I/O Operations)
+    register #(DATA_WIDTH) in_port_register (
+        .clock(Clock),
+        .clear(Clear),
+        .enable(Yin),
+        .data_in(Mdatain),
+        .data_out(In_Port)
+    );
+
+    // Constant Sign-Extended Register
+    register #(DATA_WIDTH) c_sign_extended_register (
+        .clock(Clock),
+        .clear(Clear),
+        .enable(IRin),
+        .data_in(Mdatain),
+        .data_out(C_sign_extended)
+    );
+
 	// ALU
 	ALU #(DATA_WIDTH) alu (
 		.a(R3),
 		.b(R7),
-		.control(4'd4),
-		.result(alu_result)
+		.control(control),
+		.Z_reg(alu_result)
 	);
 
 	register #(DATA_WIDTH) reg4 (
@@ -44,6 +109,17 @@ module DataPath #(parameter DATA_WIDTH = 32)(
 		.enable(R4in),
 		.data_in(alu_result),
 		.data_out(R4)
+	);
+
+	// MDR
+	MDR #(DATA_WIDTH) mdr (
+		.Clock(Clock),
+		.Clear(Clear),
+		.Read(Read),
+		.BusMuxOut(BusMuxOut),
+		.MDRin(MDRin),
+		.BusMuxIn_MDR(BusMuxIn_MDR),
+		.Mdatain(Mdatain)
 	);
 
 	//Bus
@@ -63,7 +139,8 @@ module DataPath #(parameter DATA_WIDTH = 32)(
 		.R12(R12),
 		.R13(R13),
 		.R15(R15),
-		.bus_out(Mdatain)
+		.MDR(BusMuxIn_MDR),
+		.bus_out(BusMuxOut)
 	);
 
 endmodule
--- ELEC374_PHASE1/datapath_tb.v
+++ ELEC374_PHASE1/datapath_tb.v
@@ -1,12 +1,13 @@
 // and datapath_tb.v file: <This is the filename>
 `timescale 1ns/10ps
 module datapath_tb;
-	 reg PCout, Zlowout, MDRout, R3out, R7out; // add any other signals to see in your simulation
+	 reg PCout, Zlowout, MDRout, R3out, R7out; 
 	 reg MARin, Zin, PCin, MDRin, IRin, Yin;
-	 reg IncPC, Read, AND, R3in, R4in, R7in;
-	 reg LOin, HIin, MUL, R2out, R6out; //nick multiply signals needed
+	 reg IncPC, Read, R3in, R4in, R7in;
+	 reg LOin, HIin; 
 	 reg Clock, Clear;
 	 reg [31:0] Mdatain;
+	 reg [3:0] control;
 	 parameter Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
 				 Reg_load2b = 4'b0100, Reg_load3a = 4'b0101, Reg_load3b = 4'b0110, T0 = 4'b0111,
 				 T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
@@ -27,7 +28,7 @@ DataPath DUT(
 	.Yin(Yin),
 	.IncPC(IncPC), 
 	.Read(Read),
-	.AND(AND),
+	.control(control), // may need to be deleted and replaced by control
 	.R3in(R3in),
 	.R4in(R4in), 
 	.R7in(R7in), 
@@ -64,7 +65,7 @@ always @(Present_state) // do the required job in each state
 			 PCout <= 0; Zlowout <= 0; MDRout <= 0; // initialize the signals
 			 R3out <= 0; R7out <= 0; MARin <= 0; Zin <= 0;
 			 PCin <=0; MDRin <= 0; IRin <= 0; Yin <= 0;
-			 IncPC <= 0; Read <= 0; AND <= 0;
+			 IncPC <= 0; Read <= 0; control <= 4'd0;
 			 Clear <= 0;
 			 R3in <= 0; R4in <= 0; R7in <= 0; Mdatain <= 32'h00000000;
 		end
@@ -75,7 +76,7 @@ always @(Present_state) // do the required job in each state
 			 #15 Read <= 0; MDRin <= 0; // for your current implementation
 		end
 		Reg_load1b: begin
-			 MDRout <= 1; R3in <= 1;
+			 #5 MDRout <= 1; R3in <= 1;
 			 #15 MDRout <= 0; R3in <= 0; // initialize R3 with the value 0x22
 		end
 		Reg_load2a: begin
@@ -84,11 +85,11 @@ always @(Present_state) // do the required job in each state
 			 #15 Read <= 0; MDRin <= 0;
 		end
 		Reg_load2b: begin
-			 MDRout <= 1; R7in <= 1;
+			 #5 MDRout <= 1; R7in <= 1;
 			 #15 MDRout <= 0; R7in <= 0; // initialize R7 with the value 0x24
 		end
 		Reg_load3a: begin
-			 Mdatain <= 32'h00000028;
+			 Mdatain <= 32'h0000001;
 			 Read <= 1; MDRin <= 1;
 			 #15 Read <= 0; MDRin <= 0;
 		end
@@ -110,21 +111,21 @@ always @(Present_state) // do the required job in each state
 			R3out <= 1; Yin <= 1;
 		end
 		T4: begin
-			R7out <= 1; AND <= 1; Zin <= 1;
+			R7out <= 1; control <= 4'd5; Zin <= 1;
 		end
 		T5: begin
 			Zlowout <= 1; R4in <= 1;
 		end
 	endcase
   end
-// // MAC code for simulations
-// 	initial begin
-//         $dumpfile("datapath_tb.vcd");
-//         $dumpvars();
-//     end
-// initial begin
-//     #300;  // Run for 1000 time units
-//     $display("Simulation complete.");
-//     $finish;
-// end
+// MAC code for simulations
+	initial begin
+        $dumpfile("datapath_tb.vcd");
+        $dumpvars();
+    end
+initial begin
+    #300;  // Run for 1000 time units
+    $display("Simulation complete.");
+    $finish;
+end
 endmodule 
