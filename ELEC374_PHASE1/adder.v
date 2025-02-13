314a9917542ad10cd54bc03b0c84e73f6b4c6913
Nathan <nathan.d.cook99@gmail.com>
2025-02-13 11:22:02 -0500

    Update adder.v

--- ELEC374_PHASE1/adder.v
+++ ELEC374_PHASE1/adder.v
@@ -13,14 +13,11 @@ module cla_4 #(parameter DATA_WIDTH = 32)(
 	assign G = x & y;
 	assign sum = P ^ C; //for bit wise computation
 
-	assign C[0] = cin;
-	assign C[1] = G[0] | (P[0] & C[0]);
-	assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
-	assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
-	assign C[4] = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
+//	assign C[0] = cin;
+	assign C[0] = G[0] | (P[0] & C[0]);
+	assign C[1] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
+	assign C[2] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
+	assign C[3] = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
 
-	assign cout = C[4];
+	assign cout = C[3];
 endmodule
-
-	
-	
