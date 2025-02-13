8a03ac5553b9da7d414c602625782f89ae675527
nathancook9 <nathan.d.cook99@gmail.com>
2025-02-12 23:57:03 -0500

    Change adder logic style

--- ELEC374_PHASE1/adder.v
+++ ELEC374_PHASE1/adder.v
@@ -9,18 +9,16 @@ module cla_4 #(parameter DATA_WIDTH = 32)(
 
 	wire [3:0] G, P, C;
 	
-	assign P=xor(x, y);
-	assign G=and(x, y);
-	assign sum = xor(P, C);
-	
-	
+	assign P = x ^ y;
+	assign G = x & y;
+	assign sum = P ^ C; //for bit wise computation
+
 	assign C[0] = cin;
-	assign C[1] = or(G[0], and(P[0],C[0]));
-	assign C[2] = or(G[1], or(and(P[1],G[0]), and(P[1],P[0],C[0])));
-	assign C[3] = or( or(G[2], and(P[2],G[1])), or( and(P[2],P[1],G[0]), and(P[2],P[1],P[0],G[0])) );
-	assign C[4] = or(G[3], or( or(and(P[3],G[2]), and(P[3],P[2],G[1])), or( and(P[3],P[2],P[1],G[0]), and(P[3],P[2],P[1],P[0],G[0]) ) ) );
-	
-	assign sum = and(P, C[3:0]); 
+	assign C[1] = G[0] | (P[0] & C[0]);
+	assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
+	assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
+	assign C[4] = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
+
 	assign cout = C[4];
 endmodule
 
