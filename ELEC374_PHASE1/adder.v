18e0c5628a643bd310424bea42f7386f7f632d7a
nathancook9 <nathan.d.cook99@gmail.com>
2025-02-18 23:18:40 -0500

    Fixing 32 bit adder

--- ELEC374_PHASE1/adder.v
+++ ELEC374_PHASE1/adder.v
@@ -1,7 +1,8 @@
-//full carry-look-ahead adder
+//carry-look-ahead adder (4 bit)
 
 module cla_4 #(parameter DATA_WIDTH = 32)(
-	input [3:0] x, y,
+	input [3:0] x,
+	input [3:0] y,
 	input cin,
 	output [3:0] sum,
 	output cout
@@ -11,13 +12,13 @@ module cla_4 #(parameter DATA_WIDTH = 32)(
 	
 	assign P = x ^ y;
 	assign G = x & y;
-	assign sum = P ^ C; //for bit wise computation
 
-//	assign C[0] = cin;
-	assign C[0] = G[0] | (P[0] & C[0]);
-	assign C[1] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
-	assign C[2] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
-	assign C[3] = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
+	assign C[0] = cin;
+	assign C[1] = G[0] | (P[0] & C[0]);
+	assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
+	assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
+	assign cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
 
-	assign cout = C[3];
+	assign sum = P ^ C;
+	
 endmodule
--- ELEC374_PHASE1/collective_add.v
+++ ELEC374_PHASE1/collective_add.v
@@ -1,41 +1,39 @@
 // 32 bit carry-lookahead-adder
 
-//Take my previous 4 bit adders and make 32 bit
+//Use 4 bit adders to make 16 bit
+module cla16 #(parameter DATA_WIDTH = 32) (
+	input [15:0] d1,
+	input [15:0] d2,
+	input cin,
+	output [15:0] sum,
+   output cout,
+);
+	wire cout, c1, c2, c3;
+
+
+   //Use 4 bit cla with 4 bit increments to make 16 bit adder
+   cla_4 n1(.x(d1[3:0]), .b(d2[3:0]), .cin(cin), .sum(sum[3:0]), .cout(c1));
+   cla_4 n2(.x(d1[7:4]), .b(d2[7:4]), .cin(c1), .sum(sum[7:4]), .cout(c2));
+   cla_4 n3(.x(d1[11:8]), .b(d2[11:8]), .cin(c2), .sum(sum[11:8]), .cout(c3));
+   cla_4 n4(.x(d1[15:12]), .b(d2[15:12]), .cin(c3), .sum(sum[15:12]), .cout(cout));
+	
+
+endmodule
+
+
+//Take two 16 bit adders and make 32 bit
 
 module carry_lookahead_32bit #(parameter DATA_WIDTH = 32) (
-   input [31:0] d1, d2,
+   input [31:0] d1,
+	input [31:0] d2,
 	input cin,
+	output [31:0] sum
    output cout,
-   output [31:0] sum
 );
 
-   wire c0, c1, c2, c3, c4, c5, c6, c7;
-   reg [31:0] b;
+	wire c1;
 
-   always @(*) begin
-		if (cin == 1)
-			b <= -d2;
-		else
-			b <= d2;
-	end
-
-   //Use 4 bit cla with 4 bit increments to make 32 bit adder
-   cla_4 n1(d1[3:0], b[3:0], cin, sum[3:0], c0);
-   cla_4 n2(d1[7:4], b[7:4], c0, sum[7:4], c1);
-   cla_4 n3(d1[11:8], b[11:8], c1, sum[11:8], c2);
-   cla_4 n4(d1[15:12], b[15:12], c2, sum[15:12], c3);
-   cla_4 n5(d1[19:16], b[19:16], c3, sum[19:16], c4);
-   cla_4 n6(d1[23:20], b[23:20], c4, sum[23:20], c5);
-   cla_4 n7(d1[27:24], b[27:24], c5, sum[27:24], c6);
-   cla_4 n8(d1[31:28], b[31:28], c6, sum[31:28], c7);
-
-   assign cout = c7;
-	//assign final_sum = sum;
-	 
-	//Account for carry of last add
-	//always @(*) begin
-//	if (c7 == 1 ) begin 
-//		assign sum = sum|(c7<<32);
-//	end
-
+   cla16 n1(.x(d1[15:0]), .b(d2[15:0]), .cin(cin), .sum(sum[15:0]), .cout(c1));
+	cla16 n2(.x(d1[31:16]), .b(d2[31:16]), .cin(c1), .sum(sum[31:16]), .cout(cout));
+	
 endmodule
