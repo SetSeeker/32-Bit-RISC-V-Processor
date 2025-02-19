be17df270c7ebee658a6a4eab9f495e2dde03974
nathancook9 <nathan.d.cook99@gmail.com>
2025-02-19 00:47:42 -0500

    Fix adder + my ALU componet
    (This complies and doesn't cause any errors) Didn't get to simulation testing but if anyone has time lmk how it works.

--- ELEC374_PHASE1/adder.v
+++ ELEC374_PHASE1/adder.v
@@ -1,24 +1,23 @@
 //carry-look-ahead adder (4 bit)
 
-module cla_4 #(parameter DATA_WIDTH = 32)(
-	input [3:0] x,
-	input [3:0] y,
-	input cin,
-	output [3:0] sum,
-	output cout
+module cla_4(
+   input [3:0] a,
+   input [3:0] b,
+   input cin,
+   output [3:0] sum,
+   output cout
 );
+   wire [3:0] P, G, c;
 
-	wire [3:0] G, P, C;
-	
-	assign P = x ^ y;
-	assign G = x & y;
+   assign P = a ^ b; // Propagate
+   assign G = a & b; // Generate
 
-	assign C[0] = cin;
-	assign C[1] = G[0] | (P[0] & C[0]);
-	assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
-	assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
-	assign cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
+   assign c[0] = cin;
+   assign c[1] = G[0] | (P[0] & c[0]);
+   assign c[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & c[0]);
+   assign c[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & c[0]);
+   assign cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & c[0]);
 
-	assign sum = P ^ C;
-	
+   assign sum = P ^ c;
+
 endmodule
--- ELEC374_PHASE1/collective_add.v
+++ ELEC374_PHASE1/collective_add.v
@@ -1,39 +1,35 @@
 // 32 bit carry-lookahead-adder
 
-//Use 4 bit adders to make 16 bit
+// Use 4 bit adders to make 16 bit
 module cla16 #(parameter DATA_WIDTH = 32) (
-	input [15:0] d1,
-	input [15:0] d2,
-	input cin,
-	output [15:0] sum,
-   output cout,
+    input [15:0] d1,
+    input [15:0] d2,
+    input cin,
+    output [15:0] sum,
+    output cout
 );
-	wire cout, c1, c2, c3;
+    wire c1, c2, c3;
 
-
-   //Use 4 bit cla with 4 bit increments to make 16 bit adder
-   cla_4 n1(.x(d1[3:0]), .b(d2[3:0]), .cin(cin), .sum(sum[3:0]), .cout(c1));
-   cla_4 n2(.x(d1[7:4]), .b(d2[7:4]), .cin(c1), .sum(sum[7:4]), .cout(c2));
-   cla_4 n3(.x(d1[11:8]), .b(d2[11:8]), .cin(c2), .sum(sum[11:8]), .cout(c3));
-   cla_4 n4(.x(d1[15:12]), .b(d2[15:12]), .cin(c3), .sum(sum[15:12]), .cout(cout));
-	
-
+    // Use 4 bit cla with 4 bit increments to make 16 bit adder
+    cla_4 n1(.a(d1[3:0]), .b(d2[3:0]), .cin(cin), .sum(sum[3:0]), .cout(c1));
+    cla_4 n2(.a(d1[7:4]), .b(d2[7:4]), .cin(c1), .sum(sum[7:4]), .cout(c2));
+    cla_4 n3(.a(d1[11:8]), .b(d2[11:8]), .cin(c2), .sum(sum[11:8]), .cout(c3));
+    cla_4 n4(.a(d1[15:12]), .b(d2[15:12]), .cin(c3), .sum(sum[15:12]), .cout(cout));
+    
 endmodule
 
-
-//Take two 16 bit adders and make 32 bit
-
-module carry_lookahead_32bit #(parameter DATA_WIDTH = 32) (
-   input [31:0] d1,
-	input [31:0] d2,
-	input cin,
-	output [31:0] sum
-   output cout,
+// Take two 16 bit adders and make 32 bit
+module collective_add #(parameter DATA_WIDTH = 32) (
+    input [31:0] d1,
+    input [31:0] d2,
+    input cin,
+    output [31:0] sum,
+    output cout
 );
 
-	wire c1;
+    wire c1;
 
-   cla16 n1(.x(d1[15:0]), .b(d2[15:0]), .cin(cin), .sum(sum[15:0]), .cout(c1));
-	cla16 n2(.x(d1[31:16]), .b(d2[31:16]), .cin(c1), .sum(sum[31:16]), .cout(cout));
-	
+    cla16 n1(.d1(d1[15:0]), .d2(d2[15:0]), .cin(cin), .sum(sum[15:0]), .cout(c1));
+    cla16 n2(.d1(d1[31:16]), .d2(d2[31:16]), .cin(c1), .sum(sum[31:16]), .cout(cout));
+    
 endmodule
