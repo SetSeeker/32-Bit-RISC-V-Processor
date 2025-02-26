94412e9fe98cfe189be9483360542c644bdd5b22
Nathan <nathan.d.cook99@gmail.com>
2025-02-26 17:57:57 -0500

    Better add sub tb

--- ELEC374_PHASE1/add_sub_tb.v
+++ ELEC374_PHASE1/add_sub_tb.v
@@ -101,7 +101,7 @@ always @(Present_state) // do the required job in each state
 			 enable_reg <= 16'b0; Mdatain <= 32'h00000000;
 		end
 		Reg_load1a: begin
-			 Mdatain <= 32'h00000FFF;
+			 Mdatain <= 32'hFFFFFFFF;
 			 #5 Read <= 1; MDRin <= 1; // Took out #10 for '1', as it may not be needed
 			 #15 Read <= 0; MDRin <= 0; // for your current implementation
 		end
@@ -110,7 +110,7 @@ always @(Present_state) // do the required job in each state
 			 #15 MDRout <= 0; enable_reg <= 16'b0; // initialize R3 with the value 0x22
 		end
 		Reg_load2a: begin
-			 Mdatain <= 32'h00000008;
+			 Mdatain <= 32'h00000088;
 			 #5 Read <= 1; MDRin <= 1; 
 			 #15 Read <= 0; MDRin <= 0;
 		end
@@ -131,7 +131,7 @@ always @(Present_state) // do the required job in each state
 			 PCout <= 1; MARin <= 1; control <= 5'd19; //Zin <= 1;
 			 #5 Zin <= 1;
 			 #10 MARin <= 0;  PCout <= 0;//Zin <= 1;
-			 #5 Zin <= 0; control <= 5'd3; //change to add or subtract
+			 #5 Zin <= 0; control <= 5'd0;
 		end
 		T1: begin
 			Mdatain <= 32'h2A2B8000; Zlowout <= 1;
@@ -151,7 +151,7 @@ always @(Present_state) // do the required job in each state
 			#15 R3out <= 0; Yin <= 0;
 		end
 		T4: begin
-			R7out <= 1; control <= 5'd0; 
+			R7out <= 1; control <= 5'd4; //change to add or subtract
 			#5 Zin <= 1;
 			#10 R7out <= 0;
 			#5 Zin <= 0;
