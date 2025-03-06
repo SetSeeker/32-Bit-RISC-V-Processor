120ce88bcafb78b99ae0fc8efbc5288ec2e55aa3
Nathan <nathan.d.cook99@gmail.com>
2025-03-06 10:50:20 -0500

    Zero thing

--- ELEC374_PHASE1/DataPath.v
+++ ELEC374_PHASE1/DataPath.v
@@ -8,7 +8,7 @@ module DataPath #(parameter DATA_WIDTH = 32)(
 	input R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, 
           R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out, 
           PCout, MDRout, HIout, LOout, Z_high_out, Z_low_out,
-          In_Port_out, C_sign_extended_out, RAM_read, RAM_write
+          In_Port_out, C_sign_extended_out
 );
 
     wire [DATA_WIDTH-1:0] R0, R1, R2, R3, R4, 
@@ -17,33 +17,27 @@ module DataPath #(parameter DATA_WIDTH = 32)(
                           HI, LO, Z_high, Z_low,
 
                           PC, IR, In_Port, C_sign_extended, Y,
-                          BusMuxOut, BusMuxIn_MDR;
-    
-    wire [DATA_WIDTH/4:0] MAR_address_out;
+                          BusMuxOut, BusMuxIn_MDR, MAR;
 								  
 	wire [(DATA_WIDTH*2)-1:0] ALU_result;
-
-    // Select_Encode #(DATA_WIDTH) select_encode (
-    // .IR(),
-    // .Gra(),
-    // .Grb(),
-    // .Grc(),
-    // .Rin(),
-    // .Rout(),
-    // .BAout(),
-    // .Rin_out(),
-    // .Rout_out(),
-    // .C_sign_extended()
-    // );
-
-
+	
 	register #(DATA_WIDTH) reg0 (
-    .clock(Clock),
-    .clear(Clear),
-    .enable(enable[0]),
-    .data_in(BusMuxOut),
-    .data_out(R0)
-    );
+		.clock(Clock),
+		.clear(Clear),
+		.enable(enable[0]),
+		.BAout(0),
+		.data_in(BusMuxOut),
+		.data_out(R0),
+	);
+
+
+//	register #(DATA_WIDTH) reg0 (
+//    .clock(Clock),
+//    .clear(Clear),
+//    .enable(enable[0]),
+//    .data_in(BusMuxOut),
+//    .data_out(R0)
+//    );
 
     register #(DATA_WIDTH) reg1 (
         .clock(Clock),
@@ -270,21 +264,13 @@ module DataPath #(parameter DATA_WIDTH = 32)(
 		.Mdatain(Mdatain)
 	);
 
-    MAR #(DATA_WIDTH) mar (
+    register #(DATA_WIDTH) mar (
         .clock(Clock),
         .clear(Clear),
         .enable(MARin),
         .data_in(BusMuxOut),
-        .data_out(MAR_address_out)
+        .data_out(MAR)
     );
-
-    // RAM #(DATA_WIDTH) ram (
-    //     .read(RAM_read),
-    //     .write(RAM_write),
-    //     .data_in(BusMuxIn_MDR),
-    //     .data_out(BusMuxIn_MDR),
-    //     .address(MAR_address_out)
-    // );
 
 	//Bus
 	Bus #(DATA_WIDTH) bus(
--- ELEC374_PHASE1/register.v
+++ ELEC374_PHASE1/register.v
@@ -1,5 +1,5 @@
 module register #(parameter DATA_WIDTH = 32, INIT = 32'h0)(
-	input clear, clock, enable, 
+	input clear, clock, enable, BAout, 
 	input [DATA_WIDTH-1:0] data_in,
 	output wire [DATA_WIDTH-1:0] data_out
 );
@@ -16,5 +16,6 @@ always @ (posedge clock)
 				q <= data_in;	// load data
 			end
 		end
-	assign	data_out = q[DATA_WIDTH-1:0];
+	//Add extra logic to check for R0
+	assign data_out = (q == 32'd0) ? ((~BAout) & q) : q;
 endmodule
