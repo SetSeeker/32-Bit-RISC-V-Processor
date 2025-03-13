133e642f52d74b2bc4acc9848a14b853c9c14a3b
GitNH27 <nicholas.hum27@gmail.com>
2025-03-13 16:40:30 -0400

    con_ff and tb updates

---
+++ ELEC374_PHASE1/conn_FF.v
@@ -0,0 +1,53 @@
+`timescale 1ns/10ps
+
+module CON_FF#(parameter DATA_WIDTH = 32)(
+    input [DATA_WIDTH-1:0] IR,
+    input [DATA_WIDTH-1:0] BusMuxOut,
+    input CONin,
+    output CON
+    );
+
+reg [3:0] Ra;
+reg [3:0] C2;
+reg brzr, brnz, brpl, brmi;
+reg BusOR;
+reg sigDig;
+integer i;
+reg D;
+reg CONout = 0;
+
+always @ (IR, BusMuxOut) begin    
+    Ra = IR[26:23];
+    C2 = IR[22:19]; // C2 is a 4-bit but last 2 bits are most important
+    brzr = 0;
+    brnz = 0;
+    brpl = 0;
+    brmi = 0;
+    BusOR = 0;
+    
+    //Decode IR
+    case (C2)
+        4'b0000: brzr = 1;
+        4'b0001: brnz = 1;
+        4'b0010: brpl = 1;
+        4'b0011: brmi = 1;
+    endcase
+    
+    //Determine input signals
+    sigDig = BusMuxOut[31];
+    for (i = 0; i < 32; i = i + 1) begin
+        BusOR = BusOR | BusMuxOut[i];
+    end
+    
+    // CONN FF Logic
+    D = (brzr & ~BusOR) | (brnz & BusOR) | (brpl & ~sigDig) | (brmi & sigDig);
+end
+
+always @ (posedge CONin) begin
+    #2 //System delay (bus prop delay)
+    CONout = D;
+end
+
+assign CON = CONout;
+
+endmodule
